b6d2f4f7abcfc603f2298f0bcff3af6537fbec37
Sharime Cahuas <sharimecahuas@gmail.com>
2023-03-27 00:47:21 -0300

    adding dark/light mode toggle

--- js/script.js
+++ js/script.js
@@ -26,3 +26,18 @@ $("#btn-close-text-menu").addEventListener("click", () => {
     $("#text-menu-panel").classList.add("hidden")
 })
 
+// SET LIGHT MODE
+
+$("#btn-light-mode").addEventListener("click", () => {
+        $("#btn-dark-mode").classList.remove("hidden")
+        $("#btn-light-mode").classList.add("hidden")
+        $("body").setAttribute("data-theme", "light-theme")
+})
+
+// SET DARK MODE
+
+$("#btn-dark-mode").addEventListener("click", () => {
+        $("#btn-dark-mode").classList.add("hidden")
+        $("#btn-light-mode").classList.remove("hidden")
+        $("body").removeAttribute("data-theme", "light-theme")
+})
